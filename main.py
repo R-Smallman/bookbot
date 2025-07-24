5e526505cb4cbd25d9895877b86a657ab4fcfff7
R-Smallman <richard.smallman.work@outlook.com>
2025-07-24 18:14:35 +0100

    included program user guide

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from stats import get_book_text, get_book_length, get_book_characters, get_book_
 
 import sys
 
+# program guide ;
+# run with command ... python3 main.py <filepath>
+# replacing filepath with the path to the book in txt format
+
 def main():
     # require filepath on run
     if len(sys.argv) != 2:
